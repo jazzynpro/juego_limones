9f6f7ed1335a4531bcb1e8feb0ff0c3f31124b64
jazzynpro <reader_jaz@hotmail.com>
2026-04-12 08:07:05 -0500

    mover izquierda y refrescar pantalla

--- limones.js
+++ limones.js
@@ -3,6 +3,9 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60; 
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
+
+
 
 function iniciar(){
     dibujarSuelo();
@@ -17,5 +20,19 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="#eff31c";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function moverIzquierda(){
+    personajeX= personajeX-10;
+    actualizarPantalla();
+}
+//mover derecha
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
