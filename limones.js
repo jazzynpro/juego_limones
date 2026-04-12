851d03c4b82ea99e3d737258db7979194b43d339
jazzynpro <reader_jaz@hotmail.com>
2026-04-12 12:51:29 -0500

    funcion detectar piso

--- limones.js
+++ limones.js
@@ -10,6 +10,9 @@ let limonX=canvas.width/2;
 let limonY=0;
 const ANCHO_LIMON=20;
 const ALTURA_LIMON=20;
+let vidas=3;
+
+let puntaje=0;
 
 function iniciar(){
     dibujarSuelo();
@@ -28,13 +31,13 @@ function dibujarPersonaje(){
 function moverIzquierda(){
     personajeX= personajeX-10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
 }
 //mover derecha
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
 }
 function actualizarPantalla(){
     limpiarCanva();
@@ -52,23 +55,31 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY=limonY+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
+    detectarPiso();
 }
-function detectarColision(){
+function detectarAtrapado(){
     if((limonX+ANCHO_LIMON > personajeX && 
         limonX < personajeX+ANCHO_PERSONAJE) && 
         (limonY+ALTURA_LIMON > personajeY && 
         limonY<personajeY+ALTURA_PERSONAJE) ){
         //alert("ATRAPADO!!");
         aparecerLimon();
+        puntaje=puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
+
     }
-}
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
 }
 function aparecerLimon(){
     limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
     limonY=0;
     actualizarPantalla();
+}
+function detectarPiso(){
+    if (limonY+ALTURA_LIMON == canvas.height-ALTURA_SUELO){
+        aparecerLimon();
+        vidas=vidas-1;
+        mostrarEnSpan("txtVidas",vidas);
+        alert("Perdiste!!");
+    }
 }
