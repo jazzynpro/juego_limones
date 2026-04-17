b4b6718e9df4e3c727af46d0a65a5291411d5059
jazzynpro <reader_jaz@hotmail.com>
2026-04-17 15:24:06 -0500

    personaje y limon cambio

--- limones.js
+++ limones.js
@@ -1,19 +1,25 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
-const ALTURA_PERSONAJE=60; 
-const ANCHO_PERSONAJE=40;
+const ALTURA_PERSONAJE=100; 
+const ANCHO_PERSONAJE=85;
 let personajeX=canvas.width/2;
 let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 //constantes y variables limon
 let limonX=canvas.width/2;
 let limonY=0;
 const ANCHO_LIMON=20;
-const ALTURA_LIMON=20;
+const ALTURA_LIMON=35;
 let vidas=3;
 let puntaje=0;
 let velocidadCaida=200;
 let intervalo;
+
+let imgSakura=new Image();
+imgSakura.src= "sakura.png";
+
+let imgCarta=new Image();
+imgCarta.src= "carta.jpg";
 
 function iniciar(){
     intervalo=setInterval(bajarLimon,velocidadCaida);//funcion de js recibe 2 parametros, 1 funcion y tiempo en miliseg
@@ -27,8 +33,12 @@ function dibujarSuelo(){
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
 }
 function dibujarPersonaje(){
-    ctx.fillStyle="#1325c9";
-    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.drawImage(
+        imgSakura,
+        personajeX,
+        personajeY,
+        ANCHO_PERSONAJE,
+        ALTURA_PERSONAJE);
 }
 function moverIzquierda(){
     personajeX= personajeX-10;
@@ -51,8 +61,12 @@ function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 function dibujarLimon(){
-    ctx.fillStyle="#ffffff";
-    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+    ctx.drawImage(
+        imgCarta,
+        limonX,
+        limonY,
+        ANCHO_LIMON,
+        ALTURA_LIMON);
 }
 function bajarLimon(){
     limonY=limonY+10;
